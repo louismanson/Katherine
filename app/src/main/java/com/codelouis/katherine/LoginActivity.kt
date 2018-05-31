10065f3912da9adc32db98266ee9f68012bd9aca
Louis <manson.louis@gmail.com>
2018-05-30 17:55:04 -0700

    add Firebase

---
+++ app/src/main/java/com/codelouis/katherine/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.codelouis.katherine
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Luis Hernandez on 30/mayo/2018
+ */
+
+class LoginActivity: AppCompatActivity(){
+
+}
